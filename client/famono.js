c513a82064573d7f84fc401e8fef3ca3521f6e7c
Theodor Vararu <theo@tvararu.ro>
2014-04-20 18:03:48 +0300

    Reorganize code.

--- client/famono.js
+++ client/famono.js
@@ -1,27 +1,14 @@
 UI.body.rendered = function() {
-  var Engine = require('famous/core/Engine');
-  
-  var Surface = require('famous/core/Surface');
-  var Modifier = require('famous/core/Modifier');
-  var Transform = require('famous/core/Transform');
-  
-  var Transitionable = require("famous/transitions/Transitionable");
-  var SnapTransition = require("famous/transitions/SnapTransition");
-  var SpringTransition = require("famous/transitions/SpringTransition");
-  
-  Transitionable.registerMethod('snap', SnapTransition);
-  Transitionable.registerMethod('spring', SpringTransition);
-  
   var mainContext = Engine.createContext();
-  
+
   mainContext.setPerspective(200);
-  
+
   var containerModifier = new Modifier({
     origin: [0.5, 0.5]
   });
-  
+
   mainContext = mainContext.add(containerModifier);
-  
+
   var square = new Surface({
     size: [200, 200],
     content: 'Hello.',
@@ -31,6 +18,6 @@ UI.body.rendered = function() {
       textAlign: 'center'
     }
   });
-  
+
   mainContext.add(square);
 };
--- client/lib/famous-init.js
+++ client/lib/famous-init.js
@@ -1,4 +1,29 @@
 Meteor.startup(function() {
-  require("famous-polyfills"); // Add polyfills
-  require("famous/core/famous"); // Add the default css file
+  // Add polyfills.
+  require("famous-polyfills");
+
+  // Add the default CSS file.
+  require("famous/core/famous");
+
+  // Add everything to window for simplicity's sake.
+  window.Engine = require('famous/core/Engine');
+
+  window.View = require('famous/core/View');
+  window.Surface = require('famous/core/Surface');
+  window.Modifier = require('famous/core/Modifier');
+  window.Transform = require('famous/core/Transform');
+  window.Draggable = require('famous/modifiers/Draggable');
+  window.StateModifier = require('famous/modifiers/StateModifier');
+
+  window.Scrollview = require('famous/views/Scrollview');
+  window.HeaderFooterLayout = require("famous/views/HeaderFooterLayout");
+
+  window.Easing = require('famous/transitions/Easing');
+  window.Transitionable = require('famous/transitions/Transitionable');
+  window.SnapTransition = require('famous/transitions/SnapTransition');
+  window.SpringTransition = require('famous/transitions/SpringTransition');
+
+  // Initialize two basic transitions.
+  Transitionable.registerMethod('snap', SnapTransition);
+  Transitionable.registerMethod('spring', SpringTransition);
 });
