4365cffe650762c8e508321b31ab93d4b2755f69
Theodor Vararu <theo@tvararu.ro>
2014-04-20 17:55:50 +0300

    Basic famous square.

--- client/famono.js
+++ client/famono.js
@@ -1,36 +1,36 @@
 UI.body.rendered = function() {
-  // var Engine = require('famous/core/Engine');
-  // 
-  // var Surface = require('famous/core/Surface');
-  // var Modifier = require('famous/core/Modifier');
-  // var Transform = require('famous/core/Transform');
-  // 
-  // var Transitionable = require("famous/transitions/Transitionable");
-  // var SnapTransition = require("famous/transitions/SnapTransition");
-  // var SpringTransition = require("famous/transitions/SpringTransition");
-  // 
-  // Transitionable.registerMethod('snap', SnapTransition);
-  // Transitionable.registerMethod('spring', SpringTransition);
-  // 
-  // var mainContext = Engine.createContext();
-  // 
-  // mainContext.setPerspective(200);
-  // 
-  // var containerModifier = new Modifier({
-  //   origin: [0.5, 0.5]
-  // });
-  // 
-  // mainContext = mainContext.add(containerModifier);
-  // 
-  // var square = new Surface({
-  //   size: [200, 200],
-  //   content: 'Hello.',
-  //   properties: {
-  //     backgroundColor: 'rgba(200, 200, 200, 0.5)',
-  //     lineHeight: '200px',
-  //     textAlign: 'center'
-  //   }
-  // });
-  // 
-  // mainContext.add(square);
+  var Engine = require('famous/core/Engine');
+  
+  var Surface = require('famous/core/Surface');
+  var Modifier = require('famous/core/Modifier');
+  var Transform = require('famous/core/Transform');
+  
+  var Transitionable = require("famous/transitions/Transitionable");
+  var SnapTransition = require("famous/transitions/SnapTransition");
+  var SpringTransition = require("famous/transitions/SpringTransition");
+  
+  Transitionable.registerMethod('snap', SnapTransition);
+  Transitionable.registerMethod('spring', SpringTransition);
+  
+  var mainContext = Engine.createContext();
+  
+  mainContext.setPerspective(200);
+  
+  var containerModifier = new Modifier({
+    origin: [0.5, 0.5]
+  });
+  
+  mainContext = mainContext.add(containerModifier);
+  
+  var square = new Surface({
+    size: [200, 200],
+    content: 'Hello.',
+    properties: {
+      backgroundColor: 'rgba(200, 200, 200, 0.5)',
+      lineHeight: '200px',
+      textAlign: 'center'
+    }
+  });
+  
+  mainContext.add(square);
 };
