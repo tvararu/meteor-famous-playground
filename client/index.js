f8132f5ef52f897df1b396672695f5c66e1604c8
Theodor Vararu <theo@tvararu.ro>
2014-04-21 15:12:53 +0300

    Fix .famous-container removal.

--- client/index.js
+++ client/index.js
@@ -1,5 +1,5 @@
 Template.home.rendered = function() {
   // Probably erroneous way to clear the previous Famous canvas.
   // But it works.
-  $('.famous-container').html('');
+  $('.famous-container').remove();
 };
