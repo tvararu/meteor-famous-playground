ec6ce0608447f06c07e94f410536261fb4c92f67
Theodor Vararu <theo@tvararu.ro>
2014-04-22 11:10:24 +0300

    Hack up scrolling on homepage. Haven't figured out how to deconstruct the render tree between pages.

--- client/index.js
+++ client/index.js
@@ -1,5 +1,24 @@
 Template.home.rendered = function() {
   // Probably erroneous way to clear the previous Famous canvas.
   // But it works.
-  $('.famous-container').remove();
+  var mainContext = Engine.createContext();
+
+  var contentView = new Scrollview({
+    size: [undefined, true]
+  });
+
+  window.content = new Surface({
+    size: [undefined, undefined],
+    content: UI.render(Template.homecontent).render().toHTML()
+  });
+
+  content.pipe(contentView);
+  contentView.sequenceFrom([content]);
+
+  mainContext.add(contentView);
+  
+  setTimeout(function() {
+    var h = $('.famous-container .container')[0].scrollHeight + 72;
+    content.setSize([undefined, h]);
+  }, 1000);
 };
